4b58881deb9251c1fa3b6d4a1eff0f02e5bb1172
boogiemonsta <boogiemonsta@gmail.com>
2022-07-16 10:45:57 +0200

    CORS fix

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import config from 'config';
+import cors from 'cors';
 import connect from './utils/connect';
 import routes from './routes';
 import deserializeUser from './middleware/deserializeUser';
@@ -8,12 +9,13 @@ const port = config.get<number>('port');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(deserializeUser);
 
 app.listen(port, async () => {
-	console.log(`App is running at http://localhost:${port}`);
+	console.log(`CORS-enabled server is running at http://localhost:${port}`);
 	await connect();
 	routes(app);
 });
